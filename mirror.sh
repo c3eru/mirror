b3332cf0105b8afafc1c391cb6f068fdba4bd9ef
c3eru <96016081+c3eru@users.noreply.github.com>
2022-11-10 19:08:57 +0700

    ci: Test

--- mirror.sh
+++ mirror.sh
@@ -1,9 +1,9 @@
 cd $CIRRUS_WORKING_DIR
 
-wget --show-progress --progress=bar:force --no-check-certificate "https://bigota.d.miui.com/V13.0.2.0.SGKIDXM/miui_SPESNIDGlobal_V13.0.2.0.SGKIDXM_7471370342_12.0.zip" || exit 1
+wget -q --show-progress --progress=bar:force --no-check-certificate "https://bigota.d.miui.com/V13.0.7.0.RGKIDXM/miui_SPESNIDGlobal_V13.0.7.0.RGKIDXM_5da99bf349_11.0.zip" || exit 1
 nama_file=$(basename *.zip)
 
-rclone copy *.zip mobx:MIUI -P
+rclone copy $nama_file mobx:MIUI -P
 
 echo
 echo Tautan Unduhan = https://file.cloudmobx.workers.dev/MIUI/$nama_file
